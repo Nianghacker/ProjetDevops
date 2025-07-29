6925f902f2919de1430b626d7b1a667ce1452485
Niang_Modi@hacker-etchique <101745870+Nianghacker@users.noreply.github.com>
2025-07-29 09:17:21 +0000

    Initial push

--- backend/index.js
+++
@@ -1,4 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Vote App Backend is running'));
-app.listen(3000, () => console.log('Backend running on port 3000'));
